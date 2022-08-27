3578a1323b22fe3a6eead9672639cb4af9730e75
Andrey Pakhomov <stonewalrusfan@gmail.com>
2022-08-28 00:39:56 +0300

    hooks

--- src/basics/button/index.tsx
+++ src/basics/button/index.tsx
@@ -1,6 +1,7 @@
-import { FC } from "react";
+import { FC, useState } from "react";
 import './styles.css'
 
 export const Button:FC = () => {
-	return <button className="button">test</button>
+	const [value, setValue] = useState(0);
+	return <button className="button" onClick={() => setValue((old) => old + 1)}>{value}</button>
 }
